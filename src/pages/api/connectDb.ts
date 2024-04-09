1a9e45cb05109ad8bf5211c51ea9dcbf4f93a932
javad yosuefi <javadyousefi9000@gmail.com>
2024-04-09 12:33:10 -0900

    first commit

---
+++ src/pages/api/connectDb.ts
@@ -0,0 +1,32 @@
+// pages/api/saveData.js
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
+    if (req.method === 'POST' || true) {
+        const { data } = req.body;
+
+        const client = new MongoClient(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        try {
+            await client.connect();
+            const database = client.db('modimal'); // Choose a name for your database
+            const collection = database.collection('users'); // Choose a name for your collection
+
+            const avalinbleUser = await collection.find({}).toArray()
+
+            console.log(avalinbleUser)
+            //   await collection.insertOne({ data });
+
+            res.status(201).json({ message: 'Data saved successfully!', data: avalinbleUser });
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!' });
+        } finally {
+            await client.close();
+        }
+    } else {
+        res.status(405).json({ message: 'Method not allowed!' });
+    }
+}
