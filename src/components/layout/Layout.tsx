be6b8a6085383c60957e028bde616c8d5603c08b
Javad Yousefi <javadyousefi9000@gmail.com>
2024-04-12 00:35:38 +0330

    fix build bug

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,10 +2,10 @@ import Header from "@/components/layout/Header";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <>
+    <div className="md:mt-[71px] mt-[52px]">
       <Header />
       {children}
-    </>
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import Styles from "../styles/style.module.css";
 
 export default function Home() {
-  return <main className="text-xl text-black font-bold h-[3000px]"></main>;
+  return (
+    <main className="">
+      <section className={`h-[calc(100vh-52px)] w-full`}>
+        <div className={`${Styles.dotBackground} w-full h-full `}>
+
+        </div>
+      </section>
+      <div className="h-[3000px]"> </div>
+    </main>
+  );
 }
